f3924d8df65dac2089f863a0f276858494990bd7
linkinpok <long.linkinpok@gmail.com>
2019-05-15 10:15:37 +0200

    solve a1p3 and partly a1p4

--- asgn1/skeleton/problem3.jl
+++ asgn1/skeleton/problem3.jl
@@ -1,3 +1,25 @@
 # Problem 3: Getting to know Julia
+using PyPlot
+using Colors
+using Images
+using Statistics
 
-# here you go..
+# Task 1
+img_rgb = Float64.(imread("a1p3.png"))
+img_gray = convert(Array{Float64}, Gray.(img_rgb))
+
+# However, this conversion does not work as expect, since the output still has 3 dimensions,
+# so we take over the script introduced in the Julia tutorial
+img_gray = 0.2989*img_rgb[:,:,1] + 0.5870*img_rgb[:,:,2] + 0.1140*img_rgb[:,:,3]
+
+# Task 2
+pygui(true)
+figure()
+imshow(img_gray, "gray")
+title("grayscale")
+axis("off")
+
+# Task 3
+pixel_min = minimum(img_gray)
+pixel_max = maximum(img_gray)
+pixel_mean = mean(img_gray)
--- asgn1/skeleton/problem4.jl
+++ asgn1/skeleton/problem4.jl
@@ -1,7 +1,17 @@
 # Problem 4: Stereo likelihood
+using Distributions
+using PyPlot
+
+# We take over the script introduced in the Julia tutorial
+function rgb2gray(rgb)
+    gray = 0.2989*rgb[:,:,1] + 0.5870*rgb[:,:,2] + 0.1140*rgb[:,:,3]
+    return gray
+end
 
 function load_data()
-
+    i0 = rgb2gray(Float64.(imread("i0.png")))
+    i1 = rgb2gray(Float64.(imread("i1.png")))
+    gt = Float64.(imread("gt.png")) * 255
     @assert maximum(gt) <= 16
     return i0::Array{Float64,2}, i1::Array{Float64,2}, gt::Array{Float64,2}
 end
@@ -9,7 +19,15 @@ end
 
 # Shift all pixels of i1 to the right by the value of gt
 function shift_disparity(i1::Array{Float64,2}, gt::Array{Float64,2})
-
+    id = zeros(size(i1))
+    for r in 1:size(i1,1)
+        for c in 1:size(i1,2)
+            d = Int64(round(gt[r,c]))
+            if 1 <= c+d <= size(i1,2)
+                id[r,c+d] = i1[r,c]
+            end
+        end
+    end
     @assert size(id) == size(i1)
     return id::Array{Float64,2}
 end
@@ -17,7 +35,21 @@ end
 
 # Crop image to the size of the non-zero elements of gt
 function crop_image(i::Array{Float64,2}, gt::Array{Float64,2})
-
+    top, left = [1 1]
+    bottom, right = size(gt)
+    # find top left corner
+    for r in 1:size(gt,1)
+        left = findfirst(gt[r,:].!=0)
+        if left != nothing
+            top = r
+            break
+        end
+    end
+    # find bottom right corner
+    bottom = findlast(gt[:,left].!=0)
+    right = findlast(gt[top,:].!=0)
+    # crop image
+    ic = i[top:bottom,left:right]
     return ic::Array{Float64,2}
 end
 
